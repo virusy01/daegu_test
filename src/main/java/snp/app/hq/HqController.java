b347e738d2eb7db26ef61bcaeb6a5e9767e09b1d
virusy <virusy01@daum.net>
2017-02-19 02:41:09 +0900

    협동조합 현황조회 추가

--- src/main/java/snp/app/hq/HqController.java
+++ src/main/java/snp/app/hq/HqController.java
@@ -52,102 +52,51 @@ public class HqController
 	 * @param corpSeq
 	 * @return
 	 */
-	@RequestMapping(value="/info/{CORP_SEQ}", method= RequestMethod.DELETE)
+	@RequestMapping(value="/socialinfo/{CORP_SEQ}", method= RequestMethod.DELETE)
 	public int deleteSocialInfo(@PathVariable("CORP_SEQ") Integer corpSeq)
 	{
 		return service.deleteSocialInfo(corpSeq);
 	}
 
 
-
-	
-	// 정산보고/비용현황
-	@RequestMapping(value="/monthly-report/costs", method= RequestMethod.GET)
-	public List<Map<String, Object>> findCosts(@RequestParam(name = "YEAR") int year){
-		return service.findCosts(RequestParams.map());
+	/**
+	 * 협동조합 기본정보 목록
+	 * @return
+	 */
+	@RequestMapping(value="/cooperationList")
+	public List<Map<String, Object>> findCooperationList(){
+		return service.findCooperationList();
 	}
 
-	// 정산보고/고용현황
-	@RequestMapping(value="/monthly-report/employment", method= RequestMethod.GET)
-	public List<Map<String, Object>> findEmployment(@RequestParam(name = "YEAR") int year){
-		return service.findEmployment(RequestParams.map());
+	/**
+	 * 사회적기업정보 상세정보
+	 * @return
+	 */
+	@RequestMapping(value="/cooperationInfo/{CORP_SEQ}")
+	public Map<String, Object> findCooperationnfo(@PathVariable("CORP_SEQ") Integer corpSeq){
+		return service.findCooperationInfo(corpSeq);
 	}
 
-	// 정산보고/ 수입 전체 - 수입현황 입력, 수정을 각각 세개의 서비스로 되 있던걸 각각 하나로 통합
-	@RequestMapping(value="/monthly-report/income", method= RequestMethod.GET)
-	public Map<String, Object> findIncome(@RequestParam(name = "YEAR") int year){
-		return service.findIncome(RequestParams.map());
+	/**
+	 * 협동조합 정보 저장
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping(value="/cooperationInfo", method= RequestMethod.POST)
+	public int saveCooperationInfo(@RequestBody Map<String, Object> params)
+	{
+		return service.saveCooperationInfo(params);
 	}
 
-	// 정산보고/수입-유료급식
-	@RequestMapping(value="/monthly-report/income/paidMeals", method= RequestMethod.GET)
-	public List<Map<String, Object>> findIncomePaidMeals(@RequestParam(name = "YEAR") int year){
-		return service.findIncomePaidMeals(RequestParams.map());
+	/**
+	 * 협동조합 정보 삭제
+	 * @param corpSeq
+	 * @return
+	 */
+	@RequestMapping(value="/cooperationinfo/{CORP_SEQ}", method= RequestMethod.DELETE)
+	public int deleteCooperationInfo(@PathVariable("CORP_SEQ") Integer corpSeq)
+	{
+		return service.deleteCooperationInfo(corpSeq);
 	}
 
-	// 정산보고/수입-공공급식
-	@RequestMapping(value="/monthly-report/income/publicMeals", method= RequestMethod.GET)
-	public List<Map<String, Object>> findIncomePublicMeals(@RequestParam(name = "YEAR") int year){
-		return service.findIncomePublicMeals(RequestParams.map());
-	}
-
-	// 정산보고/수입-지원금
-	@RequestMapping(value="/monthly-report/income/support", method= RequestMethod.GET)
-	public List<Map<String, Object>> findIncomeSupport(@RequestParam(name = "YEAR") int year){
-		return service.findIncomeSupport(RequestParams.map());
-	}
-
-	// 월간실적/사회공헌활동
-	@RequestMapping(value="/monthly-report/donation", method= RequestMethod.GET)
-	public List<Map<String, Object>> findDonation(@RequestParam(name = "YEAR") int year){
-		return service.findDonation(RequestParams.map());
-	}
-	// 월간실적/월평균임금
-	@RequestMapping(value="/monthly-report/salary", method= RequestMethod.GET)
-	public Map<String, Object> findSalary(@RequestParam(name = "CORP_SEQ") int corpSeq,
-										  @RequestParam(name = "YEAR") int year){
-		return service.findSalary(RequestParams.map());
-	}
-
-	// 월간운영현황
-	@RequestMapping(value="/monthly-report/operation", method= RequestMethod.GET)
-	public List<Map<String, Object>> findMonthlyOperationStatus(@RequestParam(name = "YEAR") int year){
-		return service.findMonthlyOperationStatus(RequestParams.map());
-	}
-
-	// 월간요약
-	@RequestMapping(value="/monthly-report/summary", method= RequestMethod.GET)
-	public Map<String, Object> findMonthlySummary(@RequestParam(name = "YEAR") int year){
-		return service.findMonthlySummary(RequestParams.map());
-	}
-
-	/*// 연간실적/경제적가치(재무현황)
-	@RequestMapping(value="/yearly-report/finance", method= RequestMethod.GET)
-	public List<Map<String, Object>> findFinance(@RequestParam(name = "YEAR") int year){
-		return service.findFinance(RequestParams.map());
-	}
-
-	// 연간실적/사회혁신노력
-	@RequestMapping(value="/yearly-report/socialBiz", method= RequestMethod.GET)
-	public List<Map<String, Object>> findSocialBiz(@RequestParam(name = "YEAR") int year){
-		return service.findSocialBiz(RequestParams.map());
-	}
-
-	// 연간실적/사업역량개발노력
-	@RequestMapping(value="/yearly-report/bizStudy", method= RequestMethod.GET)
-	public List<Map<String, Object>> findBizStudy(@RequestParam(name = "YEAR") int year){
-		return service.findBizStudy(RequestParams.map());
-	}*/
-
-	// 성과지표 조회
-	@RequestMapping(value="/yearly-report/kpi", method= RequestMethod.GET)
-	public List<Map<String, Object>> findKpi(@RequestParam(name = "YEAR") int year){
-		return service.findKpi(RequestParams.map());
-	}
-
-	// 전체 센터 성과지표 조회, CORP_SEQ 로 서비스에서 피벗팅 함
-	@RequestMapping(value="/yearly-report/kpiAll", method= RequestMethod.GET)
-	public Map<String, List<Map<String, Object>>> findAllCorpKpiByYear(@RequestParam(name = "YEAR") int year){
-		return service.findAllCorpKpiByYear(RequestParams.map());
-	}
 }
--- src/main/java/snp/app/hq/HqRepository.java
+++ src/main/java/snp/app/hq/HqRepository.java
@@ -19,55 +19,17 @@ public interface HqRepository
 	int updateSocialInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
 	int deleteSocialInfo(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
 
+	// 협동조합 리스트
+	//List<Map<String, Object>> findCooperationList();
+
+	// 협동조합 상세정보
+	//Map<String, Object> findCooperationInfo(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
+	//int insertCooperationInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
+	//int updateCooperationInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
+	//int deleteCooperationInfo(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
 
 
 
-
-	// 정산보고/비용현황
-	List<Map<String, Object>> findCosts(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 정산보고/고용현황
-	List<Map<String, Object>> findEmployment(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 정산보고/수입-유료급식
-	List<Map<String, Object>> findIncomePaidMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 정산보고/수입-공공급식
-	List<Map<String, Object>> findIncomePublicMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 정산보고/수입-지원금
-	List<Map<String, Object>> findIncomeSupport(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/사회공헌활동
-	List<Map<String, Object>> findDonation(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/월평균임금
-	Map<String, Object> findSalary(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간 운영실적
-	List<Map<String, Object>> findMonthlyOperationStatus(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간 요약
-	List<Map<String, Object>> findMonthlySummary1(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary2(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary3(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary4(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/경제적가치(재무현황)
-	List<Map<String, Object>> findFinance(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/사회혁신활동
-	List<Map<String, Object>> findSocialBiz(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/사업역량개발노력
-	List<Map<String, Object>> findBizStudy(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 성과지표 조회
-	List<Map<String, Object>> findKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 전체 센터 성과지표 조회, CORP_SEQ 로 서비스에서 피벗팅 함
-	List<Map<String, Object>> findAllCorpKpiByYear(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findAllCorpKpiByYear_corpInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
 
 }
 	
