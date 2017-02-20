8b4f52e303eecad3ac9d07b85ebfdbd8c6a7a883
virusy <virusy01@daum.net>
2017-02-20 19:33:37 +0900

    사회적기업,협동조합,마을기업 kpi 화면 추가

--- src/main/java/snp/app/corp/CorpRepository.java
+++ src/main/java/snp/app/corp/CorpRepository.java
@@ -10,122 +10,21 @@ import java.util.Map;
 @Repository
 public interface CorpRepository
 {
-	// 센터 기본정보
-	Map<String, Object> findCorpInfo(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
-	int insertCorpInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int updateCorpInfo(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteCorpInfo(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
 
-	// 센터 고용현황
-	Map<String, Object> findCorpEmployee(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
-	int insertCorpEmployee(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int updateCorpEmployee(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteCorpEmployee(@Param("CORP_SEQ") int corpSeq, @Param("u") User user);
+	// 사회적기업 성과지표 조회
+	List<Map<String, Object>> findSocialKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
 
-	// 월간실적/비용현황
-	List<Map<String, Object>> findCosts(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertCosts(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteCosts(@Param("p") Map<String, Object> params, @Param("u") User user);
+	// 협동조합 성과지표 조회
+	List<Map<String, Object>> findCooperationKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
 
-	// 월간실적/고용현황
-	List<Map<String, Object>> findEmployment(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertEmployment(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteEmployment(@Param("p") Map<String, Object> params, @Param("u") User user);
+	// 협동조합 성과지표 조회
+	List<Map<String, Object>> findVillageKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
 
-	// 월간실적/수입-유료급식
-	List<Map<String, Object>> findIncomePaidMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertIncomePaidMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteIncomePaidMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/수입-공공급식
-	List<Map<String, Object>> findIncomePublicMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertIncomePublicMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteIncomePublicMeals(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/수입-지원금
-	List<Map<String, Object>> findIncomeSupport(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertIncomeSupport(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteIncomeSupport(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/사회공헌활동
-	List<Map<String, Object>> findDonation(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertDonation(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteDonation(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간실적/월평균임금
-	Map<String, Object> findSalary(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertSalary(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteSalary(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간 운영실적
-	List<Map<String, Object>> findMonthlyOperationStatus(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 월간 요약
-	List<Map<String, Object>> findMonthlySummary1(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary2(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary3(@Param("p") Map<String, Object> params, @Param("u") User user);
-	List<Map<String, Object>> findMonthlySummary4(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/경제적가치(재무현황)
-	List<Map<String, Object>> findFinance(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertFinance(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteFinance(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/사회혁신활동
-	List<Map<String, Object>> findSocialBiz(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertSocialBiz(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteSocialBiz(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 연간실적/사업역량개발노력
-	List<Map<String, Object>> findBizStudy(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertBizStudy(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteBizStudy(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 성과지표 조회
-	List<Map<String, Object>> findKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	Map<String, Object> findKpi_010101(@Param("p") Map<String, Object> params, @Param("u") User user);
-	//Map<String, Object> findKpi_010102(@Param("p") Map<String, Object> params, @Param("u") User user);
-	//Map<String, Object> findKpi_010201(@Param("p") Map<String, Object> params, @Param("u") User user);
-	//Map<String, Object> findKpi_010301(@Param("p") Map<String, Object> params, @Param("u") User user);
-	//Map<String, Object> findKpi_010401(@Param("p") Map<String, Object> params, @Param("u") User user);
-	//Map<String, Object> findKpi_010402(@Param("p") Map<String, Object> params, @Param("u") User user);
+	//공통
+	Map<String, Object> findKpi_detail( @Param("LEVEL3CODE") String level3Code, @Param("p") Map<String, Object> params, @Param("u") User user);
 
 
-	/* • 3년간 매출액 평균  */
-	//Map<String, Object> findKpi_030101(@Param("p") Map<String, Object> params, @Param("u") User user);
 
-	// 성과지표 조회 중 아이콘 클릭시 상세조회
-    List<Map<String, Object>> findKpi_010101_compare(@Param("p") Map<String, Object> params, @Param("u") User user);
-	/* • 3년간 매출액 평균 등급별 건수 */
-	List<Map<String, Object>> findKpi_030101_compare(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 매뉴얼 성과지표 조회(등록용 템플릿)
-	List<Map<String, Object>> findManualKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int insertManualKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
-	int deleteManualKpi(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	Integer findCurrentYear();
-	Integer findMaxYear();
-
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpSalesAmount(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpSalesPower(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpSocialBizCount(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpSocialBizExpense(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpBizStudyCount(@Param("p") Map<String, Object> params, @Param("u") User user);
-
-	// 운영실적 추이(매출액, 인당생산성, 사업개발/자문활용 횟수, 사업개발/자문활용 금액, 교육/학습 횟수, 교육/학습 시간 - 6가지)
-	List<Map<String, Object>> findOpBizStudyTime(@Param("p") Map<String, Object> params, @Param("u") User user);
 
 
 	// 성과지표 등급별 분석 데모용
--- src/main/java/snp/app/corp/CorpService.java
+++ src/main/java/snp/app/corp/CorpService.java
@@ -19,526 +19,78 @@ public class CorpService {
 	@Autowired
 	private CorpRepository repository;
 
-	public Map<String, Object> findCorpDetail(int corpSeq) {
-		Map<String, Object> detail = new HashMap<String, Object>();
-		detail.put("basic", findCorpInfo(corpSeq));
-		detail.put("seInfo", findCorpEmployee(corpSeq));
-		return detail;
-	}
+	// 사회적기업 성과지표조회
+	public List<Map<String, Object>> findSocialKpi(Map<String, Object> params) {
+		List<Map<String, Object>> result = repository.findSocialKpi(params, Security.user());
 
-	public Map<String, Object> findCorpInfo(int corpSeq) {
-		return repository.findCorpInfo(corpSeq, Security.user());
-	}
-
-	public int saveCorpInfos(List<Map<String, Object>> params) {
-		int r = 0;
-		for (Map<String, Object> p : params) {
-			r += saveCorpInfo(p);
-		}
-		return r;
-	}
-
-	public int saveCorpInfo(Map<String, Object> params) {
-		int r;
-		Integer corpSeq = (Integer) params.get("CORP_SEQ");
-		if (corpSeq != null) {
-			//repository.deleteCorpInfo(corpSeq, Security.user());
-			r = repository.updateCorpInfo(params, Security.user());
-		} else {
-			r = repository.insertCorpInfo(params, Security.user());
-		}
-		return r;
-	}
-
-	public int deleteCorpInfo(int corpSeq) {
-		return repository.deleteCorpInfo(corpSeq, Security.user());
-	}
-
-	public Map<String, Object> findCorpEmployee(int corpSeq) {
-		return repository.findCorpEmployee(corpSeq, Security.user());
-	}
-
-	public int saveCorpEmployees(List<Map<String, Object>> params) {
-		int r = 0;
-		for (Map<String, Object> p : params) {
-			r += saveCorpEmployee(p);
-		}
-		return r;
-	}
-
-	public int saveCorpEmployee(Map<String, Object> params) {
-		int r;
-
-		int corpSeq = (int) params.get("CORP_SEQ");
-		repository.deleteCorpEmployee(corpSeq, Security.user());
-		r = repository.insertCorpEmployee(params, Security.user());
-		return r;
-	}
-
-	public int deleteCorpEmployee(Integer corpSeq) {
-		return repository.deleteCorpEmployee(corpSeq, Security.user());
-	}
-
-
-	// 월간실적/비용현황
-	public List<Map<String, Object>> findCosts(Map<String, Object> params) {
-		return repository.findCosts(params, Security.user());
-	}
-
-	public int saveCosts(List<Map<String, Object>> costs) {
-		if (costs.size() < 1) {
-			return -1;
-		}
-		repository.deleteCosts(costs.get(0), Security.user());
-
-		costs.forEach(param -> {
-			repository.insertCosts(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/고용현황
-	public List<Map<String, Object>> findEmployment(Map<String, Object> params) {
-		return repository.findEmployment(params, Security.user());
-	}
-
-	public int saveEmployment(List<Map<String, Object>> employments) {
-		if (employments.size() < 1) {
-			return -1;
-		}
-		repository.deleteEmployment(employments.get(0), Security.user());
-
-		employments.forEach(param -> {
-			repository.insertEmployment(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/전체
-	public Map<String, Object> findIncome(Map<String, Object> params) {
-		Map<String, Object> nestedData = new HashMap<>();
-
-		nestedData.put("paidMeals", findIncomePaidMeals(params));
-		nestedData.put("publicMeals", findIncomePublicMeals(params));
-		nestedData.put("support", findIncomeSupport(params));
-
-		return nestedData;
-	}
-
-	public int saveIncome(Map<String, List<Map<String, Object>>> incomes) {
-
-		saveIncomePaidMeals(incomes.get("paidMeals"));
-		saveIncomePublicMeals(incomes.get("publicMeals"));
-		saveIncomeSupport(incomes.get("support"));
-
-		return 1;
-	}
-
-	// 월간실적/수입-유료급식
-	public List<Map<String, Object>> findIncomePaidMeals(Map<String, Object> params) {
-		return repository.findIncomePaidMeals(params, Security.user());
-	}
-
-
-	public int saveIncomePaidMeals(List<Map<String, Object>> incomes) {
-		if (incomes.size() < 1) {
-			return -1;
-		}
-		repository.deleteIncomePaidMeals(incomes.get(0), Security.user());
-
-		incomes.forEach(param -> {
-			repository.insertIncomePaidMeals(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/수입-공공급식
-	public List<Map<String, Object>> findIncomePublicMeals(Map<String, Object> params) {
-		return repository.findIncomePublicMeals(params, Security.user());
-	}
-
-	public int saveIncomePublicMeals(List<Map<String, Object>> incomes) {
-		if (incomes.size() < 1) {
-			return -1;
-		}
-		repository.deleteIncomePublicMeals(incomes.get(0), Security.user());
-
-		incomes.forEach(param -> {
-			repository.insertIncomePublicMeals(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/수입-지원금
-	public List<Map<String, Object>> findIncomeSupport(Map<String, Object> params) {
-		return repository.findIncomeSupport(params, Security.user());
-	}
-
-	public int saveIncomeSupport(List<Map<String, Object>> incomes) {
-		if (incomes.size() < 1) {
-			return -1;
-		}
-		repository.deleteIncomeSupport(incomes.get(0), Security.user());
-
-		incomes.forEach(param -> {
-			repository.insertIncomeSupport(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/사회공헌활동
-	public List<Map<String, Object>> findDonation(Map<String, Object> params) {
-		return repository.findDonation(params, Security.user());
-	}
-
-	public int saveDonation(List<Map<String, Object>> donations) {
-		if (donations.size() < 1) {
-			return -1;
-		}
-		repository.deleteDonation(donations.get(0), Security.user());
-
-		donations.forEach(param -> {
-			repository.insertDonation(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 월간실적/월평균임금
-	public Map<String, Object> findSalary(Map<String, Object> params) {
-		return repository.findSalary(params, Security.user());
-	}
-
-	public int saveSalary(Map<String, Object> salary) {
-		repository.deleteSalary(salary, Security.user());
-		return repository.insertSalary(salary, Security.user());
-	}
-
-	// 월간운영현황
-	public List<Map<String, Object>> findMonthlyOperationStatus(Map<String, Object> params) {
-		return repository.findMonthlyOperationStatus(params, Security.user());
-	}
-
-	// 월간요약
-	public Map<String, Object> findMonthlySummary(Map<String, Object> params) {
-		List<Map<String, Object>> summary1 = repository.findMonthlySummary1(params, Security.user());
-		List<Map<String, Object>> summary2 = repository.findMonthlySummary2(params, Security.user());
-		List<Map<String, Object>> summary3 = repository.findMonthlySummary3(params, Security.user());
-		List<Map<String, Object>> summary4 = repository.findMonthlySummary4(params, Security.user());
-
-		Map<String, Object> nested = new HashMap<>();
-		nested.put("summary1", summary1);
-		nested.put("summary2", summary2);
-		nested.put("summary3", summary3);
-		nested.put("summary4", summary4);
-
-		return nested;
-	}
-
-
-	// 연간실적/경제적가치(재무현황)
-	public List<Map<String, Object>> findFinance(Map<String, Object> params) {
-		return repository.findFinance(params, Security.user());
-	}
-
-	public int saveFinance(List<Map<String, Object>> finances) {
-		finances.forEach(param -> {
-			repository.deleteFinance(param, Security.user());
-		});
-
-		finances.forEach(param -> {
-			repository.insertFinance(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 연간실적/사회적공헌활동
-	public List<Map<String, Object>> findSocialBiz(Map<String, Object> params) {
-		return repository.findSocialBiz(params, Security.user());
-	}
-
-	public int saveSocialBiz(List<Map<String, Object>> socialBizes) {
-		socialBizes.forEach(param -> {
-			repository.deleteSocialBiz(param, Security.user());
-		});
-
-		socialBizes.forEach(param -> {
-			repository.insertSocialBiz(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 연간실적/사업역량개발노력
-	public List<Map<String, Object>> findBizStudy(Map<String, Object> params) {
-		return repository.findBizStudy(params, Security.user());
-	}
-
-	public int saveBizStudy(List<Map<String, Object>> bizStudies) {
-		bizStudies.forEach(param -> {
-			repository.deleteBizStudy(param, Security.user());
-		});
-
-		bizStudies.forEach(param -> {
-			repository.insertBizStudy(param, Security.user());
-		});
-		return 1;
-	}
-
-	// 성과지표조회
-	public List<Map<String, Object>> findKpi(Map<String, Object> params) {
-		List<Map<String, Object>> result = repository.findKpi(params, Security.user());
 
 		result.forEach(kpiItem -> {
 			String level3Code = (String) kpiItem.get("L3_KPI_CD");
-			Map<String, Object> detailResult = getKpiDetail(level3Code, params);
+			Map<String, Object> detailResult = getKpiSocial(level3Code, params);
 			if (detailResult != null) {
-				//kpiItem.putAll(detailResult);
+				kpiItem.putAll(detailResult);
 			}
 		});
 
 		return result;
 	}
 
-	public Map<String, Object> getKpiDetail(String level3Code, Map<String, Object> params) {
+	public Map<String, Object> getKpiSocial(String level3Code, Map<String, Object> params) {
 		Map<String, Object> detailResult = null;
-		switch (level3Code) {
 
-			case "010101":
-				detailResult = repository.findKpi_010101(params, Security.user());
-				break;
-			case "010102":
-				//detailResult = repository.findKpi_010102(params, Security.user());
-				break;
-			case "010201":
-				//detailResult = repository.findKpi_010201(params, Security.user());
-				break;
-			case "010301":
-				//detailResult = repository.findKpi_010301(params, Security.user());
-				break;
-			case "010401":
-				//detailResult = repository.findKpi_010401(params, Security.user());
-				break;
-			case "010402":
-				//detailResult = repository.findKpi_010402(params, Security.user());
-				break;
-			case "020101":
-				break;
-			// 경제적 가치 창출 - 매출액 -  • 년간 매출액 평균
-			case "030101":
-				//detailResult = repository.findKpi_030101(params, Security.user());
-				break;
-			case "030201":
-				break;
-			case "030301":
-				break;
-			case "030401":
-				break;
-			case "030501":
-				break;
-			case "030601":
-				break;
-			case "040101":
-				break;
-			case "040201":
-				break;
-			case "050101":
-				break;
-			case "050201":
-				break;
-			case "060101":
-				break;
-			case "060201":
-				break;
-			case "070101":
-				break;
-			case "070102":
-				break;
-			case "070103":
-				break;
-			case "070104":
-				break;
-			case "070201":
-				break;
-			case "070202":
-				break;
-			case "070203":
-				break;
-			case "070204":
-				break;
-			case "070205":
-				break;
-			case "070206":
-
-		}
+		detailResult = repository.findKpi_detail(level3Code, params, Security.user());
 		return detailResult;
 	}
 
-	// 성과지표조회
-	public List<Map<String, Object>> findKpiCompare(Map<String, Object> params, String level3Code) {
-		List<Map<String, Object>> result = null;
-		switch (level3Code) {
-			// 사회적 가치 창출 - 공공급식 품질개선 가치 -  • 자부담 공공급식 가치=급식수×6,400원*1 (품질개선 상수)
-			case "010101":
-				//result = repository.findKpi_010101_compare(params, Security.user());
-				break;
-			case "010102":
-				//detailResult = repository.findKpi_010102(params, Security.user());
-				//kpiItem.putAll(detailResult);
-				break;
-			case "010201":
-				break;
-			case "010301":
-				break;
-			case "010401":
-				break;
-			case "010402":
-				break;
-			case "020101":
-				break;
-			// 경제적 가치 창출 - 매출액 -  • 년간 매출액 평균
-			case "030101":
-				//result = repository.findKpi_030101_compare(params, Security.user());
-				break;
-			case "030201":
-				break;
-			case "030301":
-				break;
-			case "030401":
-				break;
-			case "030501":
-				break;
-			case "030601":
-				break;
-			case "040101":
-				break;
-			case "040201":
-				break;
-			case "050101":
-				break;
-			case "050201":
-				break;
-			case "060101":
-				break;
-			case "060201":
-				break;
-			case "070101":
-				break;
-			case "070102":
-				break;
-			case "070103":
-				break;
-			case "070104":
-				break;
-			case "070201":
-				break;
-			case "070202":
-				break;
-			case "070203":
-				break;
-			case "070204":
-				break;
-			case "070205":
-				break;
-			case "070206":
 
-		}
-		return result;
-	}
+	// 협동조합 성과지표조회
+	public List<Map<String, Object>> findCooperationKpi(Map<String, Object> params) {
+		List<Map<String, Object>> result = repository.findCooperationKpi(params, Security.user());
 
-	// 매뉴얼 성과지표 조회(등록용 템플릿)
-	public List<Map<String, Object>> findManualKpi(Map<String, Object> params) {
-		List<Map<String, Object>> manualKpis = repository.findManualKpi(params, Security.user());
-		PivotTable pt = new PivotTable();
-		pt.setKeyColumns(new String[]{"CORP_SEQ", "CENTER_NM", "YEAR"});
-		pt.setPivotColumn("KPI_CD");
-		pt.setValueColumn("RESULT");
-		//pt.setNullReplace("0");
-		pt.setSourceList(manualKpis);
 
-		List<Map<String, Object>> pivoted = pt.composePivot();
-		return pivoted;
-	}
-
-	public int saveManualKpi(List<Map<String, Object>> manualKpis) {
-		// 피벗된 데이터를 원래 테이블 구조에 맞게 풀어냄.
-		manualKpis.forEach(param -> {
-			Map<String, Object> newParam = new HashMap<>();
-			Iterator<String> keyIterator = param.keySet().iterator();
-			// HashMap은 key가 정렬되지 않았으므로 pivot 데이터 이외의 key값을 먼저 분리해낸다.
-			while (keyIterator.hasNext()) {
-				String key = keyIterator.next();
-				// 피벗 데이터(x축)는 key-value값 분리
-				if ("YEAR".equals(key) || "CORP_SEQ".equals(key) || "CENTER_NM".equals(key)) {
-					newParam.put(key, param.get(key));
-				}
-			}
-
-			// 피벗데이터를 만날때마다 데이터를 등록한다(CORP_SEQ, KPI_CD, RESULT)
-			keyIterator = param.keySet().iterator();
-			while (keyIterator.hasNext()) {
-				String key = keyIterator.next();
-				// 피벗 데이터(x축)는 key-value값 분리
-				if (!"YEAR".equals(key) && !"CORP_SEQ".equals(key) && !"CENTER_NM".equals(key)) {
-					newParam.put("KPI_CD", key);
-					newParam.put("RESULT", param.get(key));
-					repository.deleteManualKpi(newParam, Security.user());
-				}
+		result.forEach(kpiItem -> {
+			String level3Code = (String) kpiItem.get("L3_KPI_CD");
+			Map<String, Object> detailResult = getKpiCooperation(level3Code, params);
+			if (detailResult != null) {
+				kpiItem.putAll(detailResult);
 			}
 		});
 
-		manualKpis.forEach(param -> {
-			Map<String, Object> newParam = new HashMap<>();
-			Iterator<String> keyIterator = param.keySet().iterator();
-
-			// HashMap은 key가 정렬되지 않았으므로 pivot 데이터 이외의 key값을 먼저 분리해낸다.
-			while (keyIterator.hasNext()) {
-				String key = keyIterator.next();
-				// 피벗 데이터(x축)는 key-value값 분리
-				if ("YEAR".equals(key) || "CORP_SEQ".equals(key) || "CENTER_NM".equals(key)) {
-					newParam.put(key, param.get(key));
-				}
-			}
-
-			// 피벗데이터를 만날때마다 데이터를 등록한다(CORP_SEQ, KPI_CD, RESULT)
-			keyIterator = param.keySet().iterator();
-			while (keyIterator.hasNext()) {
-				String key = keyIterator.next();
-				// 피벗 데이터(x축)는 key-value값 분리
-				if (!"YEAR".equals(key) && !"CORP_SEQ".equals(key) && !"CENTER_NM".equals(key)) {
-					newParam.put("KPI_CD", key);
-					newParam.put("RESULT", param.get(key));
-					repository.insertManualKpi(newParam, Security.user());
-				}
-			}
-		});
-		return 1;
+		return result;
 	}
 
-	// 월간운영실적 추이
-	public List<Map<String, Object>> findOpTrend(Map<String, Object> params, String kpiKind) {
-		List<Map<String, Object>> result;
-		switch (kpiKind) {
-			case "0":
-				result = repository.findOpSalesAmount(params, Security.user());
-				break;
-			case "1":
-				result = repository.findOpSalesPower(params, Security.user());
-				break;
-			case "2":
-				result = repository.findOpSocialBizCount(params, Security.user());
-				break;
-			case "3":
-				result = repository.findOpSocialBizExpense(params, Security.user());
-				break;
-			case "4":
-				result = repository.findOpBizStudyCount(params, Security.user());
-				break;
-			default: // "5"
-				result = repository.findOpBizStudyTime(params, Security.user());
-				break;
-		}
+	public Map<String, Object> getKpiCooperation(String level3Code, Map<String, Object> params) {
+		Map<String, Object> detailResult = null;
+
+		detailResult = repository.findKpi_detail(level3Code, params, Security.user());
+		return detailResult;
+	}
+
+
+
+	// 마을기업 성과지표조회
+	public List<Map<String, Object>> findVillageKpi(Map<String, Object> params) {
+		List<Map<String, Object>> result = repository.findVillageKpi(params, Security.user());
+
+
+		result.forEach(kpiItem -> {
+			String level3Code = (String) kpiItem.get("L3_KPI_CD");
+			Map<String, Object> detailResult = getKpiVillage(level3Code, params);
+			if (detailResult != null) {
+				kpiItem.putAll(detailResult);
+			}
+		});
 
 		return result;
 	}
+
+	public Map<String, Object> getKpiVillage(String level3Code, Map<String, Object> params) {
+		Map<String, Object> detailResult = null;
+
+		detailResult = repository.findKpi_detail(level3Code, params, Security.user());
+		return detailResult;
+	}
+
 
 
 	// 성과지표 등급별 분석 데모용
@@ -570,4 +122,5 @@ public class CorpService {
 	public List<Map<String, Object>>chartDemo3(Map<String, Object> params,String corpKind) {
 		return repository.chartDemo3(params,Security.user());
 	}
+
 }
