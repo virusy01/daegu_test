deacb1ee0a60a567e8881561038aa3ea1c7d0b99
Woomun Jung <woomun.jung@gmail.com>
2017-02-26 10:43:05 +0000

    WAR 패키징

--- src/main/java/snp/DaeguApplication.java
+++ src/main/java/snp/DaeguApplication.java
@@ -2,11 +2,19 @@ package snp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("snp")
-public class DaeguApplication {
+public class DaeguApplication extends SpringBootServletInitializer {
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.setProperty("file.encoding", "UTF-8");
+		return application.sources(DaeguApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaeguApplication.class, args);
