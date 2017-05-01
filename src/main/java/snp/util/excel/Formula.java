235b9aa1aa7c308fd43e8bff43adf19e9356ed28
virusy <virusy01@daum.net>
2017-05-01 18:13:22 +0900

    전체기업 상세정보 조회 평균 계산 로직 수정

--- src/main/java/snp/util/excel/Formula.java
+++ src/main/java/snp/util/excel/Formula.java
@@ -27,23 +27,45 @@ public class Formula {
                         sumItem.put(columnName, value);
                     }
                 } catch (Exception e){
-                    System.out.println("Column Name:[" + columnName + "] is not number in item index : " + source.indexOf(item));
+                    //System.out.println("Column Name:[" + columnName + "] is not number in item index : " + source.indexOf(item));
                 }
             }
         });
         return sumItem;
     }
 
+    public static Map<String, Object> size(List<Map<String, Object>> source, String[] columns){
+        Map<String, Object> countItem = new HashMap<>();
+
+        source.forEach(item -> {
+            for (String columnName : columns){
+                try {
+                    Double value = Double.valueOf(item.get(columnName).toString());
+                    // if (value != null) {
+                    int count = (int) countItem.get(columnName) + 1;
+                    countItem.put(columnName, count);
+                    System.out.println(columnName + " : " + count);
+                    // }
+                } catch (Exception e){
+                    //System.out.println("Column Name:[" + columnName + "] is not number in item index : " + source.indexOf(item));
+                }
+            }
+
+        });
+        return countItem;
+    }
     public static Map<String, Object> avg(List<Map<String, Object>> source, String[] columns){
         Map<String, Object> sumItem = Formula.sum(source, columns);
+        Map<String, Object> countItem = Formula.size(source, columns);
         Map<String, Object> avgItem = new HashMap<>();
         for (String columnName : columns){
             try {
                 double sumValue = ((Double) sumItem.get(columnName)).doubleValue();
-                double avgValue = sumValue / source.size();
+                double avgValue = sumValue / (int) countItem.get(columnName);
                 avgItem.put(columnName, Double.valueOf(Math.round(avgValue)));
+                System.out.println("size : " + countItem.get(columnName));
             } catch (Exception e) {
-                System.out.println("Column Name:[" + columnName + "] is not number" );
+                //System.out.println("Column Name:[" + columnName + "] is not number" );
             }
         }
         return avgItem;
