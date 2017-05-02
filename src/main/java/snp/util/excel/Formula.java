cd0789e1201c5f9d4423b9edeca4af81e36c4074
virusy <virusy01@daum.net>
2017-05-02 11:25:00 +0900

    전체기업 상세정보 조회 엑셀 평균 로직 수정 완료

--- src/main/java/snp/util/excel/Formula.java
+++ src/main/java/snp/util/excel/Formula.java
@@ -34,41 +34,41 @@ public class Formula {
         return sumItem;
     }
 
-    public static Map<String, Object> size(List<Map<String, Object>> source, String[] columns){
+    public static Map<String, Object> countNotNull(List<Map<String, Object>> source, String[] columns){
         Map<String, Object> countItem = new HashMap<>();
-
         source.forEach(item -> {
             for (String columnName : columns){
-                try {
-                    Double value = Double.valueOf(item.get(columnName).toString());
-                    if (value != null){
+                Object value = item.get(columnName);
+                if (value != null && !"".equals(value.toString().trim())) {
+                    try {
+                        Double.valueOf(value.toString());
                         if (countItem.containsKey(columnName)) {
-                            double added = ((Double) countItem.get(columnName)).doubleValue() + 1;
-                            countItem.put(columnName, Double.valueOf(added));
-                            System.out.println("added: " + Double.valueOf(added));
+                            Integer added = Integer.parseInt(countItem.get(columnName).toString()) + 1;
+                            countItem.put(columnName, added);
                         } else {
                             countItem.put(columnName, 1);
                         }
                     }
-                } catch (Exception e){
-                    //System.out.println("Column Name:[" + columnName + "] is not number in item index : " + source.indexOf(item));
+                    catch(Exception e){
+                        //System.out.println("Column Name:[" + columnName + "] is not number in item index : " + source.indexOf(item));
+                    }
                 }
             }
-
         });
         return countItem;
     }
+
     public static Map<String, Object> avg(List<Map<String, Object>> source, String[] columns){
         Map<String, Object> sumItem = Formula.sum(source, columns);
-        Map<String, Object> countItem = Formula.size(source, columns);
+        Map<String, Object> countItem = Formula.countNotNull(source, columns);
         Map<String, Object> avgItem = new HashMap<>();
         for (String columnName : columns){
             try {
                 double sumValue = ((Double) sumItem.get(columnName)).doubleValue();
-                double countValue = ((Double) countItem.get(columnName)).doubleValue();
+                Integer countValue = (Integer) countItem.get(columnName);
                 double avgValue = sumValue / countValue;
                 avgItem.put(columnName, Double.valueOf(Math.round(avgValue)));
-                System.out.println("size : " + countValue);
+
             } catch (Exception e) {
                 System.out.println("Column Name:[" + columnName + "] is not number" );
             }
